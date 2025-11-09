194a0374309890696ca3455c3ad4775be75d62dc
Jonatas <anjos92489@gmail.com>
2025-11-09 08:58:50 -0300

    add cadastrar-funcionario.php

---
+++ cadastrar-funcionario.php
@@ -0,0 +1,20 @@
+<h1>Cadastrar Funcionário</h1>
+
+<form action='?page=salvar-funcionario' method='POST'>
+    <input type="hidden" name='acao' value='cadastrar'>
+    <div class='mb-3'>
+        <label for="exampleFormControlInput1" class="form-label">Nome:</label>
+        <input type='text' name='nome_funcionario'  class='form-control border-success' id="exampleFormControlInput1" placeholder='Digite seu nome'>
+    </div>
+    <div class='mb-3'>
+        <label>E-mail:</label>
+        <input type='email' name='email_funcionario' class='form-control border-success' placeholder='Digite seu email'>
+    </div>
+    <div class='mb-3'>
+        <label>Telefone:</label>
+        <input type='text' name='telefone_funcionario' class='form-control border-success' placeholder='Digite sua senha'>
+    </div>
+    <div>
+        <button type='submit' class='btn btn-primary'>Enviar</button>
+    </div>
+</form>
