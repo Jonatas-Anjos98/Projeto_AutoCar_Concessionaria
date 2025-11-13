5ecb26e3cc01a861bc5caad0ca62760ee3d94aee
Jonatas <anjos92489@gmail.com>
2025-11-13 01:42:37 -0300

    add salvar-funcionario.php

---
+++ salvar-funcionario.php
@@ -0,0 +1,43 @@
+<?php 
+    switch($_REQUEST['acao']) {
+        case 'cadastrar':
+            $nome = $_POST['nome_funcionario'];
+            $email = $_POST['email_funcionario'];
+            $telefone = $_POST['telefone_funcionario'];
+            
+            $sql = "INSERT INTO funcionario (nome_funcionario, email_funcionario, telefone_funcionario) VALUES ( '{$nome}', '{$email}', '{$telefone}' )";
+            
+            $res = $conn->query($sql);
+            
+            if($res == true){
+                print "<script>alert('Cadastro realizado com sucesso!')</script>";
+                print "<script>location.href='?page=listar-funcionario';</script>";
+            }else {
+                print "<script>alert('Não foi possível realizar o cadastro!')</script>";
+                print "<script>location.href='?page=listar-funcionario';</script>";
+            }
+            
+            break;
+        case 'editar':
+            $nome = $_POST['nome_funcionario'];
+            $email = $_POST['email_funcionario'];
+            $telefone = $_POST['telefone_funcionario'];
+            
+            $sql = "UPDATE funcionario SET nome_funcionario='{$nome}', email_funcionario='{$email}', telefone_funcionario='{$telefone}' WHERE id_funcionario=" . $_REQUEST['id_funcionario'];
+
+            $res = $conn->query($sql);
+
+            if($res == true) {
+                print "<script>alert('Informações salvas com sucesso!');</script>";
+                print "<script>location.href='?page=listar-funcionario';</script>";
+            }else {
+                print "<script>alert('Não foi possivel editar as alterações!');</script>";
+                print "<script>location.href='?page=listar-funcionario';</script>";
+            }
+            
+            break;
+        case 'excluir':
+        
+            break;
+    }
+?>
