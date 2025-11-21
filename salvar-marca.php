72246083fafa92220a9e91dfbab0ee18e01a9c25
Jonatas <anjos92489@gmail.com>
2025-11-21 19:43:54 -0300

    update salvar-marca.php finished register

--- salvar-marca.php
+++ salvar-marca.php
@@ -1,24 +1,23 @@
 <?php 
-    switch($_REQUEST['acao']){
+    switch($_REQUEST['acao']){  
         case 'cadastrar':
             $nome = $_POST['nome_marca'];
 
-            $sql = "INSERT INTO marca (nome_marca) VALUES ('{$nome})";
+            $sql = "INSERT INTO marca ( nome_marca ) VALUES ( '{$nome}')";
 
             $res = $conn->query($sql);
 
-            if (){
-                print "<script>alert('Marca foi salva com sucesso!')</script>";
-                print "<script>location.href?page=listar-marca.php</script>";
+            if ($res == true){
+                print "<script>alert('Marca foi salva com sucesso!');</script>";
+                print "<script>location.href='?page=listar-marca';</script>";
             }else {
-                print "<script>alert('Não foi possivel salvar a marca!')</script>";
-                print "<script>location.href?page=listar-marca.php</script>";
+                print "<script>alert('Não foi possível salvar a marca!');</script>";
+                print "<script>location.href='?page=listar-marca';</script>";
             }
-
             break;
 
         case 'editar':
-            
+
             break;
 
         case 'excluir': 
