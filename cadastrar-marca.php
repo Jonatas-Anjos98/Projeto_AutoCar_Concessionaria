3a2c9e59699ca38a5ebd96d78102c839b4929e8a
Jonatas <anjos92489@gmail.com>
2025-11-21 13:49:00 -0300

    add cadastrar-marca.php

---
+++ cadastrar-marca.php
@@ -0,0 +1,11 @@
+<h1>Cadastrar Marca</h1>
+
+<form action="?page=salvar-cliente.php" method="POST">
+    <input type="hidden" name="acao" value="cadastrar">
+
+    <div class="mb-3">
+        <label for="exampleFormControlInput1"></label>
+        <input type="text" nmae="nome-marca" class='form-control border-success' id="exampleFormControlInput1" placeholder="Digite o nome da marca" required>
+        
+    </div><br><br><br><br><br><br><br><br><br><br>
+</form>
