41c49322fdf302ed5b479b29073bfdf8bbaac8ba
Jonatas <anjos92489@gmail.com>
2025-11-22 06:50:28 -0300

    feat(editar): agora pode ser editado a marca

--- editar-marca.php
+++ editar-marca.php
@@ -1,1 +1,22 @@
 <h1>Editar Marca</h1>
+
+<?php 
+    $sql = "SELECT * FROM marca WHERE id_marca=" . $_REQUEST['id_marca'];
+
+    $res = $conn->query($sql);
+
+    $row = $res->fetch_object();
+?>
+
+<form action='?page=salvar-marca' method="POST">
+    <input type="hidden" name='acao' value="editar">
+    <input type="hidden" name="id_marca" value="<?php print $row->id_marca; ?>">
+
+    <div class="mb-3">
+        <label for="exampleFormControlInput1"></label>
+        <input type="text" name="nome_marca" class='form-control border-success' id="exampleFormControlInput1" value='<?php print $row->nome_marca; ?>' required>
+    </div>
+    <div>
+        <button type='submit' class='btn btn-primary'>Enviar</button>
+    </div><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+</form>
