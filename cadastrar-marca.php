4b835583f4f83fad0f9cdfb7a2f3cf25d8a5b024
Jonatas <anjos92489@gmail.com>
2025-11-24 05:00:05 -0300

    style(layout): adiciona espaçamento após formulário

--- cadastrar-marca.php
+++ cadastrar-marca.php
@@ -4,10 +4,10 @@
     <input type="hidden" name='acao' value="cadastrar">
 
     <div class="mb-3">
-        <label for="exampleFormControlInput1"></label>
+        <label for="exampleFormControlInput1">Nome:</label>
         <input type="text" name="nome_marca" class='form-control border-success' id="exampleFormControlInput1" placeholder="Digite o nome da marca" required>
     </div>
     <div>
         <button type='submit' class='btn btn-primary'>Enviar</button>
-    </div><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+    </div><br><br><br><br><br><br><br><br><br>
 </form>
