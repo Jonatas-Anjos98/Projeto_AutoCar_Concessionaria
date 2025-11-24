881f58d7d9350555c379f6e34d17c0d1236d2310
Jonatas <anjos92489@gmail.com>
2025-11-24 06:46:07 -0300

    feat(backend): adicionado salvamento de modelos ao projeto

--- salvar-modelo.php
+++ salvar-modelo.php
@@ -1,5 +1,5 @@
 <?php 
-    switch ($_REQUEST['acao']){
+    switch ($_REQUEST["acao"]){
         case 'cadastrar':
             $nome = $_POST['nome_modelo'];
             $cor = $_POST['cor_modelo'];
@@ -7,20 +7,28 @@
             $tipo = $_POST['tipo_modelo'];
             $marca = $_POST['marca_id_marca'];
 
-            $sql = "INSERT INTO modelo (nome_modelo, cor_modelo, ano_modelo, tipo_modelo, marca_id_modelo) VALUES ('{$nome}', '{$cor}', '{$ano}', '{$tipo}', {$Marca})";
+            $sql = "INSERT INTO modelo (nome_modelo, cor_modelo, ano_modelo, tipo_modelo, marca_id_marca) VALUES ('{$nome}', '{$cor}', '{$ano}', '{$tipo}', {$marca})";
 
             $res = $conn->query($sql);
 
             if($res == true){
-                print "<script>alert('Essa modelo foi cadastrada!')</script>";
-                print "<script>location.href</script>";
+                print "<script>alert('Essa modelo foi cadastrado!')</script>";
+                print "<script>location.href='?page=listar-modelo';</script>";
             }else{
                 print "<script>alert('Não foi possível cadastrar esse modelo!')</script>";
-                print "<script></script>";
+                print "<script>location.href='?page=listar-modelo'</script>";
             }
 
             break;
         case 'editar':
+            $nome = $_POST['nome_modelo'];
+            $cor = $_POST['cor_modelo'];
+            $ano = $_POST['ano_modelo'];
+            $tipo = $_POST['tipo_modelo'];
+            $marca = $_POST['marca_id_marca'];
+
+
+
             break;
         case 'excluir':
             break;
