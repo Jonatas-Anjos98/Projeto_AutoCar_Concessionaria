e37f53cf44c0b189dbba554d2184898b49d11bfb
Jonatas <anjos92489@gmail.com>
2025-11-24 14:10:28 -0300

    feat(vendas): implementa cadastro de vendas no banco

---
+++ salvar-venda.php
@@ -0,0 +1,28 @@
+<?php 
+    switch ($_REQUEST['acao']){
+        case 'cadastrar':
+            $data = $_POST['data_venda'];
+            $valor = $_POST['valor_venda'];
+            $modelo = $_POST['modelo_id_modelo'];
+            $funcionario = $_POST['funcionario_id_funcionario'];
+            $cliente = $_POST['cliente_id_cliente'];
+
+            $sql = "INSERT INTO venda (data_venda, valor_venda, modelo_id_modelo, funcionario_id_funcionario, cliente_id_cliente) VALUES ('{$data}', '{$valor}', '{$modelo}', '{$funcionario}', {$cliente})"; 
+
+            $res = $conn->query($sql);
+
+            if($res == true){
+                print "<script>alert('Essa venda foi cadastrada com sucesso!');</script>";
+                print "<script>location.href='?page=listar-venda';</script>";
+            }else{
+                print "<script>alert('Não foi possível cadastrar essa venda!')</script>";
+                print "<script>location.href='?page=listar-venda';</script>";
+            }
+
+            break;
+        case 'editar':
+            break;
+        case 'excluir':
+            break;
+    }
+?>
