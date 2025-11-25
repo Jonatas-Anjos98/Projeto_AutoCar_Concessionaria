3147351f56e7c2cea75a9c12f2178059d5cbe725
Jonatas <anjos92489@gmail.com>
2025-11-25 00:56:59 -0300

    feat(vendas): implementa edição  de vendas no banco

--- salvar-venda.php
+++ salvar-venda.php
@@ -21,6 +21,23 @@
 
             break;
         case 'editar':
+            $data = $_POST['data_venda'];
+            $valor = $_POST['valor_venda'];
+            $modelo = $_POST['modelo_id_modelo'];
+            $funcionario = $_POST['funcionario_id_funcionario'];
+            $cliente = $_POST['cliente_id_cliente'];
+
+            $sql = "UPDATE venda SET data_venda='{$data}', valor_venda='{$valor}', modelo_id_modelo='{$modelo}', funcionario_id_funcionario='{$funcionario}', cliente_id_cliente='{$cliente}' WHERE id_venda=" . $_REQUEST['id_venda'];
+
+            $res = $conn->query($sql);
+
+            if($res == true){
+                print "<script>alert('Essa venda foi atualizada com sucesso!');</script>";
+                print "<script>location.href='?page=listar-venda';</script>";
+            }else{
+                print "<script>alert('Essa venda não foi atualizada!');</script>";
+                print "<script>location.href='?page=listar-venda';</script>";
+            }
             break;
         case 'excluir':
             break;
